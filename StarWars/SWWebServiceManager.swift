2b0f98846e5dbd5d8bb2ba05996669e3fe32c0ee
Quinto Technologies <quintotechnologies@Quintos-MacBook-Pro.local>
2018-03-07 02:41:59 +0530

    Add class SWWebServiceManager to handle network task.

---
+++ StarWars/SWWebServiceManager.swift
@@ -0,0 +1,38 @@
+//
+//  SWWebServiceManager.swift
+//  StarWars
+//
+//  Created by Quinto Technologies on 06/03/18.
+//  Copyright © 2018 HarpreetSingh. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class SWWebServiceManager {
+
+    private static let baseURL = "https://swapi.co/api"
+    
+    private static func processRequest(endPoint: ApiEndPoint, param: Parameters, completion: @escaping (SWServiceError?, Data?) -> Void) {
+        
+
+        if (!NetworkStatus.sharedInstance.isReachable) {
+            completion(SWServiceError.noInternetConnection, nil)
+            return
+        }
+        Alamofire.request("\(baseURL)\(endPoint.path)", method: endPoint.method, parameters: param, encoding: URLEncoding.default, headers: nil).responseData(completionHandler: { (response) in
+            
+            if let error = response.error {
+                completion(SWServiceError.custom(error.localizedDescription), nil)
+            } else if let data = response.result.value {
+                completion(nil, data)
+            } else {
+                completion(SWServiceError.other, nil)
+            }
+        })
+    }
+}
+
+
+
+
