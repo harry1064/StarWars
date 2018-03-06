42268030302176d869f789b383f691e85409f92b
Quinto Technologies <quintotechnologies@Quintos-MacBook-Pro.local>
2018-03-07 02:46:31 +0530

    Add enum SWPeopleApiEndPoint to define end point for Star Wars People Api.

---
+++ StarWars/Enum/SWPeopleApiEndPoint.swift
@@ -0,0 +1,29 @@
+//
+//  SWPeopleApiEndPoint.swift
+//  StarWars
+//
+//  Created by Quinto Technologies on 07/03/18.
+//  Copyright © 2018 HarpreetSingh. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+enum SWPeopleApiEndPoint: ApiEndPoint {
+    case getPeoples
+    
+    var path: String {
+        switch self {
+        case .getPeoples:
+            return "/people"
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .getPeoples:
+            return .get
+        }
+    }
+    
+}
